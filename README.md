b0dfda2122ddaceea558b9c8f46f860f5b1d0689
Michał Bogucki <michal.bogucki@ihsmarkit.com>
2019-11-29 14:28:09 +0100

    Git init

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # structured-logging-efk
-structured logging with serilog and showing logs with the efk stack. Alerting provided by open distro
+Welcome to structured logging with serilog using efk stack. Alerting provided by open distro
+
+#1. 
+Open you project in VS code
+
+#2.
+Run the command `docker-compose up` up to run containers
+
+#3.
+Run command `docker-compose down` to turn off running containers
+
+*If any changes are made to the pproject then run `docker-compose up --build`*
---
+++ serilogapp/serilogapp/Program.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace serilogapp
+{
+    using Serilog;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Application Starting Up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception e)
+            {
+                Log.Fatal(e, "The application failed to start correctly.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog()
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
