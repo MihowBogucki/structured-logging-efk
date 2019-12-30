f73bf2c4bd41d14cf7bce6929d59a832f49543e4
Michał Bogucki <michal.bogucki@ihsmarkit.com>
2019-12-30 11:10:43 +0100

    UI Changes

--- serilogapp/serilogapp/Pages/Index.cshtml.cs
+++ serilogapp/serilogapp/Pages/Index.cshtml.cs
@@ -24,11 +24,16 @@ namespace serilogapp.Pages
             Message = "Welcome";
 
         }
+        public void OnPostChromeIngestLogs()
+        {
+            Message = "Chrome Ingest Logs generated.";
+            ChromeIngestLogs();
+        }
+        
         public void OnPostLogs()
         {
             Message = "Random logs generated.";
             GenerateRandomLogs();
-
         }
 
         public void OnPostGenerateErrors()
@@ -36,8 +41,25 @@ namespace serilogapp.Pages
             Message = "Error logs generated.";
             GenerateErrorLogs();
         }
+        public void OnPostGenerateApiErrors()
+        {
+            Message = "API Error logs generated.";
+            GenerateApiErrors();
+        }
 
+        private void ChromeIngestLogs()
+        {
+            var chromeIngestLog = new ChromeIngest
+            {
+                App = "Incentives",
+                Component = "chrome-ingest",
+                DateTime = DateTime.UtcNow,
+                Count = 148411,
+            };
 
+            this.logger.Information("Total count of incentives to process: {@structuredLog}", chromeIngestLog);
+
+        }
         private void GenerateRandomLogs()
         {
             this.logger.Information(("You requested the Index page."));
@@ -68,6 +90,46 @@ namespace serilogapp.Pages
         }
 
         private void GenerateErrorLogs()
+        {
+            var random = new Random();
+            var numberOfErrors = random.Next(1, 10);
+
+            for (int i = 0; i < numberOfErrors; i++)
+            {
+                var randomBrand = GetRandomBrand();
+
+                var structuredIncentivesLog = new IncentivesError
+                {
+                    BrandName = randomBrand,
+                    IncentiveId = Guid.NewGuid(),
+                    DateTime = DateTime.UtcNow,
+                    App = "Incentives",
+                    Message = "The specified argument (resources) cannot be empty.Parameter name: resources",
+                    StackTract = "at System.Requires.NotNullOrEmpty[T](IEnumerable`1 collection, String name)" +
+                                 "at AM.Resources.ResourceCollection`2..ctor(IEnumerable`1 resources)" +
+                                 "at AM.Resources.Extensions.EnumerableExtensions.AsResourceCollection[TResource, TKey](IEnumerable`1 resources)" +
+                                 "at AM.Resources.Extensions.EnumerableExtensions.AsResourceCollection[TResource, TKey](IEnumerable`1 resources, String method, String requestUri)" +
+                                 "at Microsoft.AspNetCore.Mvc.ResourceCollectionOkResult`2.ExecuteResultAsync(ActionContext context)" +
+                                 "at Microsoft.AspNetCore.Mvc.Internal.ResourceInvoker.InvokeResultAsync(IActionResult result)" +
+                                 "at Microsoft.AspNetCore.Mvc.Internal.ResourceInvoker.InvokeResultFilters()" +
+                                 "at Microsoft.AspNetCore.Mvc.Internal.ResourceInvoker.InvokeNextResourceFilter()" +
+                                 "at Microsoft.AspNetCore.Mvc.Internal.ResourceInvoker.Rethrow(ResourceExecutedContext context)" +
+                                 "at Microsoft.AspNetCore.Mvc.Internal.ResourceInvoker.Next(State & next, Scope & scope, Object & state, Boolean & isCompleted)" +
+                                 "at Microsoft.AspNetCore.Mvc.Internal.ResourceInvoker.InvokeFilterPipelineAsync()" +
+                                 "at Microsoft.AspNetCore.Mvc.Internal.ResourceInvoker.InvokeAsync()" +
+                                 "at Microsoft.AspNetCore.Builder.RouterMiddleware.Invoke(HttpContext httpContext)" +
+                                 "at AM.Metrics.AspNetCore.Middleware.ErrorTrackingMiddleware.InvokeAsync(HttpContext context)" +
+                                 "at AM.Metrics.AspNetCore.Middleware.ContentLengthTrackingMiddleware.InvokeAsync(HttpContext context)" +
+                                 "at AM.Metrics.AspNetCore.Middleware.ActiveRequestTrackingMiddleware.InvokeAsync(HttpContext context)" +
+                                 "at AM.Metrics.AspNetCore.Middleware.ApdexTrackingMiddleware.InvokeAsync(HttpContext context)" +
+                                 "at AM.Metrics.AspNetCore.Middleware.RequestTrackingMiddleware.InvokeAsync(HttpContext context)" +
+                                 "at Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http.HttpProtocol.ProcessRequests[TContext](IHttpApplication`1 application)"
+                };
+
+                this.logger.Error("Generated Error log. {@structuredLog}", structuredIncentivesLog);
+            }
+        }
+        private void GenerateApiErrors()
         {
             var random = new Random();
             var numberOfErrors = random.Next(1, 10);
